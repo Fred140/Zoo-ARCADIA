5a4a486680c89235395494af6492345dac8c3277
Frederic ROUSSET <frederic.rousset@hotmail.fr>
2024-05-28 12:19:07 +0200

    update et add accountempl

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,8 +7,10 @@ export const allRoutes = [
     new Route("/habitats", "Les habitats", "/pages/habitats.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
     new Route("/avis", "Avis", "/pages/avis.html"),
-    new Route("/connexion", "Connexion Staff", "/pages/connexionstaff.html"),
-    new Route("/signupstaff", "Inscription Staff", "/pages/signupstaff.html"),
+    new Route("/connexion", "Connexion Staff", "/pages/auth/connexionstaff.html"),
+    new Route("/signupstaff", "Inscription Staff", "/pages/auth/signupstaff.html"),
+    new Route("/accountempl", "Mon compte", "/pages/auth/accountempl.html"),
+    new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
