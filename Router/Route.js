ebd9bfb4ed731c53bc2390dd7021b7f400714cfb
Frederic ROUSSET <frederic.rousset@hotmail.fr>
2024-06-04 09:27:27 +0200

    Authentification dans routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,23 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
-    }
+      this.authorize = authorize;    }
 }
+
+
+/*
+12
+[] -> Tout le monde peut y accéder
+13
+["disconnected"] -> Réserver aux utilisateurs déconnecté 
+14
+["client"] -> Réserver aux utilisateurs avec le rôle client 
+15
+["admin"] -> Réserver aux utilisateurs avec le rôle admin 
+16
+["admin", "client"] -> Réserver aux utilisateurs avec le rôle client OU admin
+17
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,20 +2,20 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/services", "Les services", "/pages/services.html"),
-    new Route("/habitats", "Les habitats", "/pages/habitats.html"),
-    new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/avis", "Avis", "/pages/avis.html"),
-    new Route("/connexion", "Connexion Staff", "/pages/auth/connexionstaff.html", "/js/auth/connexionstaff.js"),
-    new Route("/signupstaff", "Inscription Staff", "/pages/auth/signupstaff.html"),
-    new Route("/accountempl", "Mon compte Employé", "/pages/auth/accountempl.html"),
-    new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
-    new Route("/accountvete", "Mon compte Vétérinaire", "/pages/auth/accountvete.html"),
-    new Route("/dashboard", "Dashboard du Zoo", "/pages/dashboard/dashboard.html"), 
-    new Route("/gestioncompte", "Gestion des comptes pro", "/pages/gestioncompte/gestioncompte.html"),  
-    new Route("/accountadmin", "Mon compte Administrateur", "/pages/auth/accountadmin.html", "/js/auth/accountadmin.js"),
-    new Route("/loginadmin", "Connexion Administrateur", "/pages/auth/loginadmin.html", "/js/auth/loginadmin.js"),         
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/services", "Les services", "/pages/services.html", []),
+    new Route("/habitats", "Les habitats", "/pages/habitats.html", []),
+    new Route("/contact", "Contact", "/pages/contact.html", []),
+    new Route("/avis", "Avis", "/pages/avis.html", []),
+    new Route("/connexion", "Connexion Staff", "/pages/auth/connexionstaff.html", ["disconnected"], "/js/auth/connexionstaff.js"),
+    new Route("/signupstaff", "Inscription Staff", "/pages/auth/signupstaff.html", ["disconnected"]),
+    new Route("/accountempl", "Mon compte Employé", "/pages/auth/accountempl.html", ["employé"]),
+    new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html", ["admin"]),
+    new Route("/accountvete", "Mon compte Vétérinaire", "/pages/auth/accountvete.html", ["vétérinaire"]),
+    new Route("/dashboard", "Dashboard du Zoo", "/pages/dashboard/dashboard.html", ["admin"], ["vétérinaire"], ["employé"]), 
+    new Route("/gestioncompte", "Gestion des comptes pro", "/pages/gestioncompte/gestioncompte.html", ["admin"]),  
+    new Route("/accountadmin", "Mon compte Administrateur", "/pages/auth/accountadmin.html", ["admin"], "/js/auth/accountadmin.js"),
+    new Route("/loginadmin", "Connexion Administrateur", "/pages/auth/loginadmin.html", ["disconnected"], "/js/auth/loginadmin.js"),         
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
